e8d947601905993f34148438d930dd7ab8f1a81f
V-assim <wassim.robot@gmail.com>
2021-02-28 18:06:35 +0100

    argparsing

--- main.py
+++ main.py
@@ -60,7 +60,121 @@ def animate(frame):
             #ax.add_patch(plt.Arrow(c_A.real, c_A.imag, c_C.real, c_C.imag, width=0.3))
     pl.set_data(cum_orbits[-1,:int(frame)+1].real, cum_orbits[-1,:int(frame)+1].imag)
     return ax.patches+ [pl,]
-    #return pl,
+if __name__=="__main__":
+    
+    def color_parser(color):
+        match = re.match("#[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$", color)
+        if not match.group():
+            raise ValueError(f"color {color} is invalid.")
+        
+        return match.group(0)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_path",
+                        help="Path of the image to use.",
+                        type=str)
+    
+    parser.add_argument("N_points",
+                        help="Number of points to sample from the image.",
+                        default=100,
+                        type=int)
+    parser.add_argument("N_vectors",
+                        help="Number of Fourier components to use (frequencies in [-N_vectors//2 + 1; N_vectors//2])",
+                        default=100,
+                        type=int)
+    
+    parser.add_argument("--threshold_path_length",
+                        help="Threshold for path length below which paths are not considered (to avoid artifacts).",
+                        default=1e-3,
+                        type=float)
+    parser.add_argument("--threshold_vectors_module",
+                        help="Threshold for vectors module below which vectors are not drawn (for speed and visibility contraints).",
+                        default=1e-2,
+                        type=float)
+    
+    parser.add_argument("--plot",
+                        help="Plot the animation with matplotlib.",
+                        action="store_true")
+    parser.add_argument("--interval",
+                        help="Interval between two frames (in milliseconds).",
+                        default=1,
+                        type=int)
 
-ani = FuncAnimation(fig, func=animate, frames=N_samples*t, interval=1, blit=True)
+    # Video related arguments
+    parser.add_argument("-v", "--video",
+                        help="Generate a video output and saves it at given path.",
+                        type=str)
+    parser.add_argument("--fps",
+                        help="Frame rate for the generated video.",
+                        default=24,
+                        type=int)
+    parser.add_argument("--dpi",
+                        help="Dots per inch in the generated video.",
+                        default=100,
+                        type=int)
+    parser.add_argument("--width",
+                        help="Nb of pixels in width in the generated video.",
+                        type=int)
+    parser.add_argument("--height",
+                        help="Nb of pixels in height in the generated video.",
+                        type=int)
+    # Layout related arguments
+    ##  Background
+    parser.add_argument("--background_color",
+                        help="Color of the background in hex format (e.g. '#FFFFFF' or None for transparent).",
+                        default="#ffffff",
+                        type=color_parser)
+    
+    ##  Line
+    parser.add_argument("--line_color",
+                        help="Color of the drawn line in hex format (e.g. '#FFFFFF' or '#FFFFFFFF' with alpha channel).",
+                        default="#1f77b4",
+                        type=color_parser)
+    parser.add_argument("--line_width",
+                        help="Width of the drawn line.",
+                        default=0.8,
+                        type=float)
+    parser.add_argument("--line_decay",
+                        help="Decay speed of line opacity.")
+    
+    ##  Circles
+    parser.add_argument("--circle_color",
+                        help="Color of the circles in hex format (e.g. '#FFFFFF' or '#FFFFFFFF' with alpha channel).",
+                        default="#000000",
+                        type=color_parser)
+    parser.add_argument("--circle_width",
+                        help="Width of the circles.",
+                        default=0.5,
+                        type=float)
+    
+    ##  Vectors
+    parser.add_argument("--vector_color",
+                        help="Color of the vectors in hex format (e.g. '#FFFFFF' or '#FFFFFFFF' with alpha channel).",
+                        default="#000000",
+                        type=color_parser)
+    parser.add_argument("--vector_width",
+                        help="Width of the vectors.",
+                        default=0.5,
+                        type=float)
+    
+    args = parser.parse_args()
+    
+    file_path = "/home/wes/Downloads/output_ju_2.svg"
+    paths = get_paths(args.image_path)
+    shape = order_paths(paths)
+    orbits, cum_orbits = compute_fourier_serie(shape)
+    ani = plot_fourier(orbits, cum_orbits)
+    
+    if (args.width == None) ^ (args.height == None):
+        raise ValueError("Specify BOTH width and height for the plot.")
+    
+    if args.video:
+        with tqdm(total=args.N_points) as pbar:
+            if args.video.split(".")[-1] == "mp4":
+                ani.save(args.video, fps=args.fps, progress_callback=lambda x, y: pbar.update(1), writer="ffmpeg", dpi=args.dpi, extra_args=["-threads", "0"])
+        
+            elif args.video.split(".")[-1] == "gif":
+                ani.save(args.video, fps=args.fps, progress_callback=lambda x, y: pbar.update(1), writer="pillow", dpi=args.dpi, extra_args=["-threads", "0"])
+
+    if args.plot:
 plt.show()
